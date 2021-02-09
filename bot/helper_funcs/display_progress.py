60ff4bbd44e24e507fe3395156d76164ffa47a00
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-02-09 17:46:55 +0530

    Add files via upload

--- bot/helper_funcs/display_progress.py
+++ bot/helper_funcs/display_progress.py
@@ -4,43 +4,36 @@
 
 # the logging things
 import logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 import math
 import os
 import time
-import json
 
-from bot import (
-    FINISHED_PROGRESS_STR,
-    UN_FINISHED_PROGRESS_STR,
-    DOWNLOAD_LOCATION
-)
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
 
 
 async def progress_for_pyrogram(
     current,
     total,
-    bot,
     ud_type,
     message,
     start
 ):
     now = time.time()
     diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
+    if round(diff % 4.00) == 0 or current == total:
         # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
-        status = DOWNLOAD_LOCATION + "/status.json"
-        if os.path.exists(status):
-            with open(status, 'r+') as f:
-                statusMsg = json.load(f)
-                if not statusMsg["running"]:
-                    bot.stop_transmission()
         speed = current / diff
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
@@ -49,9 +42,9 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n📊 <b>Progress:</b> {2}%\n".format(
-            ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
-            ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
+        progress = "[{0}{1}] \nP: {2}%\n".format(
+            ''.join(["▪" for i in range(math.floor(percentage / 10))]),
+            ''.join(["▫" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
         tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
@@ -62,20 +55,12 @@ async def progress_for_pyrogram(
             estimated_total_time if estimated_total_time != '' else "0 s"
         )
         try:
-            if not message.photo:
-                await message.edit_text(
-                    text="{}\n {}".format(
-                        ud_type,
-                        tmp
-                    )
+            await message.edit(
+                text="{}\n {}".format(
+                    ud_type,
+                    tmp
                 )
-            else:
-                await message.edit_caption(
-                    caption="{}\n {}".format(
-                        ud_type,
-                        tmp
-                    )
-                )
+            )
         except:
             pass
 
@@ -102,5 +87,6 @@ def TimeFormatter(milliseconds: int) -> str:
     tmp = ((str(days) + "d, ") if days else "") + \
         ((str(hours) + "h, ") if hours else "") + \
         ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "")
+        ((str(seconds) + "s, ") if seconds else "") + \
+        ((str(milliseconds) + "ms, ") if milliseconds else "")
     return tmp[:-2]
